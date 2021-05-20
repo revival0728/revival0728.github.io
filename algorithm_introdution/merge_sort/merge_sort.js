3f9c1806bbdd14f8ea7b8f3a1a774d5884390f51
revival0728 <revival0728@gmail.com>
2021-05-20 11:12:54 +0800

    update content of ALG

--- algorithm_introdution/merge_sort/merge_sort.js
+++ algorithm_introdution/merge_sort/merge_sort.js
@@ -1,20 +1,39 @@
 data = {
     err_msg: "ERR_MSG >> Invalid Type",
-    code: "function merge_sort(l, r) { // [l, r) \n     // sort global array \"arr\" \n     var mid = parseInt((l+r)/2); \n     if(l == r-1) { \n         return; \n     } else { \n         merge_sort(l, mid); \n         merge_sort(mid, r); \n     } \n     al = []; \n     ar = []; \n     for(var i = l; i &lt mid; ++i) al.push(arr[i]); \n     for(var i = mid; i &lt r; ++i) ar.push(arr[i]); \n     var il = 0, ir = 0; \n     while(il &lt al.length && ir &lt ar.length) { \n         if(al[il] &lt ar[ir]) \n             arr[l++] = al[il++]; \n         else \n             arr[l++] = ar[ir++]; \n     } \n     while(il &lt al.length) arr[l++] = al[il++]; \n     while(ir &lt ar.length) arr[l++] = ar[ir++]; \n}",
-    hls: ["while", "var", "function", "if", "else", "for", "return", "+", "-", "*", "/", "=", "!", "%", "&gt", "&lt", "^", ":", ",", "&", "|", ".", ";"]
+    code: ["function merge_sort(l, r) { // [l, r) \n     // sort global array \"arr\" \n     var mid = parseInt((l+r)/2); \n     if(l == r-1) { \n         return; \n     } else { \n         merge_sort(l, mid); \n         merge_sort(mid, r); \n     } \n     al = []; \n     ar = []; \n     for(var i = l; i &lt mid; ++i) al.push(arr[i]); \n     for(var i = mid; i &lt r; ++i) ar.push(arr[i]); \n     var il = 0, ir = 0; \n     while(il &lt al.length && ir &lt ar.length) { \n         if(al[il] &lt ar[ir]) \n             arr[l++] = al[il++]; \n         else \n             arr[l++] = ar[ir++]; \n     } \n     while(il &lt al.length) arr[l++] = al[il++]; \n     while(ir &lt ar.length) arr[l++] = ar[ir++]; \n}"
+    ,"#include &ltvector&gt \nusing namespace std; \n \nvoid merge_sort(vector&ltint&gt& arr, int l, int r) { \n    int mid = (l+r)/2; \n    if(l == r-1) { \n        return; \n    } else { \n        merge_sort(arr, l, mid); \n        merge_sort(arr, mid, r); \n    } \n    vector&ltint&gt vl(arr.begin()+l, arr.begin()+mid), vr(arr.begin()+mid, arr.begin()+r); \n    auto it = arr.begin()+l, il = vl.begin(), ir = vr.begin(); \n    while(il != vl.end() && ir != vr.end()) { \n        if(*il &lt *ir) \n            *it++ = *il++; \n        else \n            *it++ = *ir++; \n    } \n    while(il != vl.end()) *it++ = *il++; \n    while(ir != vr.end()) *it++ = *ir++; \n}"
+    ,],
+    hls: [["while", "var", "function", "if", "else", "for", "return", "+", "-", "*", "/", "=", "!", "%", "&gt", "&lt", "^", ":", ",", "&", "|", ".", ";"]
+    ,["#", "include", "define", "using", "namespace", ";", "int", "float", "double", "&lt", "&gt", "for", "while", "if", "else", "long", "ll", "vector", "template", "|", "cin", "cout", "class", "+", "-", "*", "/", "=", "%", "!", "void", "new", ",", "&", ":", "return"],
+    ,],
+    ncc: ["//", "//"],
+    lan: ["javascript", "cpp"],
 };
 
 var arr = [];
 
 function setup() {
     gen_random_array();
-    print_code();
+    print_code(data.code[0], data.hls[0], data.ncc[0], "code");
+    update_sel();
 }
 
 function main() {
     spd_str = reader();
     if(!array_input(spd_str)) return;
     printer();
+}
+
+function change_lan(sel) {
+    var index = sel.selectedIndex;
+    print_code(data.code[index], data.hls[index], data.ncc[index], "code");
+}
+
+function update_sel() {
+    opt = "";
+    for(var i = 0; i < data.lan.length; ++i)
+        opt += "<option>" + data.lan[i] + "</option>";
+    document.getElementById("lan_sel").innerHTML = opt;
 }
 
 function gen_random_array() {
@@ -50,6 +69,7 @@ function printer() {
     during = (now_time-pre_time)/(1e6);
     during = during.toFixed(2);
     o += "[" + arr.join(", ") + "]\n\n";
+    o += "\n>> Language: javascript\n";
     o += ">> Used " + during + " second";
     if(during != 0 && during != 1) o += "s";
     document.getElementById("output").value = o;
@@ -63,48 +83,47 @@ function invisible(str) {
     return "<a class=\"invisible\">" + str + "</a>";
 }
 
-function print_code() {
+function min(a, b) {
+    if(a > b) return b;
+    else return a;
+}
+
+function print_code(code, hls, ncc, target) { // use <pre></pre> !!!
     opt = "<ol class=\"code_line\">";
-    lines = split(data.code, "\n");
+    lines = split(code, "\n");
     for(var lns = 0; lns < lines.length; ++lns) {
         opt += "<li class=\"code\">"
-        words = split(lines[lns], " ")
-        var isnc = false;
-        for(var i = 0; i < words.length; ++i) {
-            var is_hi = false;
-            if(words[i] == "//")
-                isnc = true;
-            if(isnc) {
-                opt += invisible(words[i]) + " ";
-                continue;
-            } 
-            if(data.hls.includes(words[i])) {
-                opt += hightlight(words[i]);
-                is_hi = true;
-            }
-            if(!is_hi) {
-                for(var j = 0; j < words[i].length; ++j) {
-                    var klen = -1;
-                    for(var k = 0; k < data.hls.length; ++k) {
-                        if(j+data.hls[k].length > words[i].length)
-                            continue;
-                        if(words[i].substr(j, data.hls[k].length) == data.hls[k])
-                            klen = data.hls[k].length;
-                    }
-                    if(klen > 0) {
-                        opt += hightlight(words[i].substr(j, klen));
-                        j += (klen-1);
-                    } else {
-                        opt += words[i][j];
-                    }
+        var ncid = 1e9;
+        var each = lines[lns], beh_id = [], beh_pos = [];
+        for(var i = 0; i < each.length; ++i) {
+            for(var k = 0; k < hls.length; ++k) {
+                if(i+2 <= each.length) {
+                    if(each.substr(i, 2) == "//")
+                        ncid = min(ncid, i);
+                }
+                if(i+hls[k].length <= each.length) {
+                    if(each.substr(i, hls[k].length) == hls[k] && beh_pos[beh_pos.length-1] != i)
+                        beh_pos.push(i), beh_id.push(k);
                 }
             }
-            opt += " ";
+        }
+        var id = 0;
+        for(var j = 0; j < each.length; ++j) {
+            if(ncid <= j) {
+                opt += invisible(each[j]);
+            }
+            else if(j == beh_pos[id]) {
+                opt += hightlight(each.substr(j, hls[beh_id[id]].length));
+                j += (hls[beh_id[id]].length-1);
+                ++id;
+            } else {
+                opt += each[j];
+            }
         }
         opt += "</li>";
     }
     opt += "</ol>";
-    document.getElementById("code").innerHTML = opt;
+    document.getElementById(target).innerHTML = opt;
 }
 
 function checker(value, index, array) {
